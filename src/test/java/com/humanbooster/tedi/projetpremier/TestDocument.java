900ff979bb3af29b6e597fbdf7c44ccd86032b5b
Jaouad <jaouad.Aqli1@gmail.com>
2015-03-26 16:21:17 +0100

    création document

---
+++ src/test/java/com/humanbooster/tedi/projetpremier/TestDocument.java
@@ -0,0 +1,15 @@
+package com.humanbooster.tedi.projetpremier;
+
+import com.humanbooster.tedi.tapp.Document;
+
+public class TestDocument {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Document doc1 = new Document(" doc1", 1, 1);
+		Document doc2 = new Document(" doc1", 1, 2);
+
+	}
+
+}
