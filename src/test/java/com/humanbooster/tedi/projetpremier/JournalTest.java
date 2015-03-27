f72c07547e360e6755be51ed169d94ffe0174788
William Olliso <will.olliso2@gmail.com>
2015-03-27 13:24:32 +0100

    JournalTest template

--- src/test/java/com/humanbooster/tedi/projetpremier/JournalTest.java
+++ src/test/java/com/humanbooster/tedi/projetpremier/JournalTest.java
@@ -1,9 +1,56 @@
+/**
+ * 
+ */
 package com.humanbooster.tedi.projetpremier;
 
-import org.junit.Assert;
-import org.junit.Before;
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
+/**
+ * @author StaggerLee
+ *
+ */
 public class JournalTest {
+
+	/**
+	 * Test method for {@link com.humanbooster.tedi.tapp.Journal#Journal()}.
+	 */
+	@Test
+	public void testJournal() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.humanbooster.tedi.tapp.Journal#createMemo()}.
+	 */
+	@Test
+	public void testCreateMemo() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.humanbooster.tedi.tapp.Journal#editMemo()}.
+	 */
+	@Test
+	public void testEditMemo() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.humanbooster.tedi.tapp.Journal#viewMemo()}.
+	 */
+	@Test
+	public void testViewMemo() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.humanbooster.tedi.tapp.Journal#deleteMemo()}.
+	 */
+	@Test
+	public void testDeleteMemo() {
+		fail("Not yet implemented");
+	}
 
 }
