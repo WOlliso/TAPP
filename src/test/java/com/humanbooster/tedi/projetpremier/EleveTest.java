799fa59f1d0befdce6766c0bdb98b84991ce971e
Jaouad <jaouad.Aqli1@gmail.com>
2015-03-27 15:01:05 +0100

    modif sur connexionPgSql et sa classe Test

--- src/test/java/com/humanbooster/tedi/projetpremier/EleveTest.java
+++ src/test/java/com/humanbooster/tedi/projetpremier/EleveTest.java
@@ -1,5 +1,35 @@
 package com.humanbooster.tedi.projetpremier;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class EleveTest {
+	Connection connection;
+
+	@Before
+	public void init() {
+		ConnexionH2 connexionH2 = new ConnexionH2();
+		this.connection = connexionH2.connexion();
+	}
+
+	@Test
+	public void testSomething() {
+		Assert.assertTrue(true);
+	}
+
+	@After
+	public void end() {
+		try {
+			this.connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
