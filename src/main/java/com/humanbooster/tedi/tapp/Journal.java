c642950f19836829834da5eb399f015e20915073
William Olliso <will.olliso2@gmail.com>
2015-03-27 15:42:34 +0100

    journal methods being built, and tests

--- src/main/java/com/humanbooster/tedi/tapp/Journal.java
+++ src/main/java/com/humanbooster/tedi/tapp/Journal.java
@@ -1,8 +1,18 @@
 package com.humanbooster.tedi.tapp;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Journal {
 
 	String name;
+	String url = "jdbc:postgresql://localhost:5432/tapp";
+	String user = "user";
+	String password = "formation";
+	ConnexionPgSql connection = new ConnexionPgSql(url, user, password);
+	Connection con = connection.connexion();
 
 	// Constructor method
 	public Journal() {
@@ -10,8 +20,19 @@ public class Journal {
 	}
 
 	// Create a new memo
-	public void createMemo() {
+	public void createMemo(String name, Connection con) {
+		try {
+			final Statement stmt = con
+					.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+							ResultSet.CONCUR_UPDATABLE);
+			final String insert = "INSERT INTO cahier_journal (nom_memo) VALUES('"
+					+ name + "')";
+			stmt.executeUpdate(insert);
 
+		} catch (final SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	// Edit an existing memo
