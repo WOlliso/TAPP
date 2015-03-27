b836dc1bcfa3a0a6f73c0f01eae1328e404e1325
William Olliso <will.olliso2@gmail.com>
2015-03-27 10:44:49 +0100

    pom.xml updated junit to version 4.12 journal.java added 4 methods

--- src/main/java/com/humanbooster/tedi/tapp/Journal.java
+++ src/main/java/com/humanbooster/tedi/tapp/Journal.java
@@ -2,4 +2,24 @@ package com.humanbooster.tedi.tapp;
 
 public class Journal {
 
+	// Create a new memo
+	public void createMemo() {
+
+	}
+
+	// Edit an existing memo
+	public void editMemo() {
+
+	}
+
+	// View an existing memo
+	public void viewMemo() {
+
+	}
+
+	// Delete a memo
+	public void deleteMemo() {
+
+	}
+
 }
