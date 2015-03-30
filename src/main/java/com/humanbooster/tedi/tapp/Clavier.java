19186b99a7bf3f4a5d05101cc7fb370b6c4da9dd
Nicolas Valadier <nicolas.valadier@gmail.com>
2015-03-30 14:32:41 +0200

    Nouvelle classe console

--- src/main/java/com/humanbooster/tedi/tapp/Clavier.java
+++ src/main/java/com/humanbooster/tedi/tapp/Clavier.java
@@ -3,25 +3,27 @@ package com.humanbooster.tedi.tapp;
 import java.util.Scanner;
 
 public class Clavier {
+	private static Scanner in = new Scanner(System.in);
+
 	// saisie clavier d'un nombre
 	public double liredouble() {
-		Scanner sc = new Scanner(System.in);
-		double nombre = sc.nextDouble();
+		// Scanner sc = new Scanner(System.in);
+		double nombre = in.nextDouble();
 		return nombre;
 	}
 
 	// Saisie chaine de caracteres au clavier
 	public String lirechaine() {
-		Scanner in = new Scanner(System.in);
+		// Scanner in = new Scanner(System.in);
 		String chaine;
-		chaine = in.nextLine();
+		chaine = in.next();
 		return chaine;
 	}
 
 	// saisie clavier d'un nombre
 	public int lirentier() {
-		Scanner sc = new Scanner(System.in);
-		int nombre = sc.nextInt();
+		// Scanner sc = new Scanner(System.in);
+		int nombre = in.nextInt();
 		return nombre;
 	}
 }
--- src/main/java/com/humanbooster/tedi/tapp/Document.java
+++ src/main/java/com/humanbooster/tedi/tapp/Document.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Document {
+
 	String url = "jdbc:postgresql://localhost:5432/tapp";
 	String user = "user";
 	String password = "formation";
