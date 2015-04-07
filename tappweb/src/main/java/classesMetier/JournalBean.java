0d50bdd14962248007be46cf7afb821b2c034917
William Olliso <will.olliso2@gmail.com>
2015-04-07 10:01:45 +0200

    Wrote getters and setters for journal class

--- tappweb/src/main/java/classesMetier/JournalBean.java
+++ tappweb/src/main/java/classesMetier/JournalBean.java
@@ -6,6 +6,25 @@ import javax.enterprise.inject.Model;
 
 @Model
 public class JournalBean {
+	private String name;
+	private Date date;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
 	public void createMemo(String name, Date date) {
 
 	}
