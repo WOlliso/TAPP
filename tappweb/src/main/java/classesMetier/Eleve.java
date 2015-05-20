b7c36578f8d3b49e0b2fde0258780096075c5888
grmt <gerente.mt@gmx.fr>
2015-05-20 15:47:41 +0200

    changement eleve

--- tappweb/src/main/java/classesMetier/Eleve.java
+++ tappweb/src/main/java/classesMetier/Eleve.java
@@ -1,19 +1,18 @@
 package classesMetier;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.List;
+import javax.enterprise.inject.Model;
 
+@Model
 public class Eleve {
 	private String nom;
 	private String prenom;
-	private Classe classe;
+	private String classe;
 
 	public Eleve() {
 
 	}
 
-	public Eleve(final String nom, final String prenom, final Classe classe) {
+	public Eleve(final String nom, final String prenom, final String classe) {
 		super();
 		this.nom = nom;
 		this.prenom = prenom;
@@ -37,64 +36,12 @@ public class Eleve {
 		this.prenom = prenom;
 	}
 
-	public Classe getClasse() {
+	public String getClasse() {
 		return classe;
 	}
 
-	public void setClasse(final Classe classe) {
+	public void setClasse(final String classe) {
 		this.classe = classe;
 	}
 
-	/*
-	 * 
-	 * public static final String INSERT_ELEVES =
-	 * "INSERT INTO eleves (nom, prenom,classe) VALUES(?,?,?)"; public static
-	 * final String SELECT_ELEVES = "SELECT * FROM eleves"; public static final
-	 * String SELECT_ELEVE_PAR_NOM = "SELECT * FROM eleves WHERE nom= ?"; public
-	 * static final String SELECT_ELEVES_PAR_CLASSE =
-	 * "SELECT * FROM eleves WHERE classe= ?"; public static final String
-	 * UPDATE_CLASSE_ELEVES="UPDATE eleves SET classe = ? WHERE nom = ?"; public
-	 * static final String
-	 * UPDATE_ELEVES="UPDATE eleves SET nom=? prenom=? classe=? WHERE nom = ?";
-	 * public static final String
-	 * DELETE_ELEVES="DELETE FROM eleves WHERE nom = ?";
-	 */
-
-	public Eleve insertEleve(final String nom, final String prenom,
-			final Classe classe) throws SQLException {
-
-		final PreparedStatement preparedStatement = ServicesDAO
-				.preparedStatement(Requests.INSERT_ELEVE);
-		preparedStatement.setString(1, nom);
-		preparedStatement.setString(2, prenom);
-		preparedStatement.setObject(3, classe);
-		preparedStatement.executeUpdate();
-
-		return new Eleve(nom, prenom, classe);
-	}
-
-	public List<Eleve> listEleves() {
-		return null;
-	}
-
-	public Eleve selectEleveParNom() {
-
-		return null;
-	}
-
-	public Eleve selectEleveParClasse() {
-		return null;
-	}
-
-	public boolean updateEleveParNom() {
-		return false;
-	}
-
-	public boolean updateClasseEleve() {
-		return false;
-	}
-
-	public boolean deleteEleve() {
-		return false;
-	}
 }
--- tappweb/src/main/java/classesMetier/EleveDAO.java
+++ tappweb/src/main/java/classesMetier/EleveDAO.java
@@ -3,7 +3,7 @@ package classesMetier;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.ArrayList;
 
 public class EleveDAO extends ServicesDAO {
 	public static final String INSERT_ELEVES = "INSERT INTO eleves (nom,prenom,classe) VALUES(?,?,?)";
@@ -13,21 +13,46 @@ public class EleveDAO extends ServicesDAO {
 	public static final String SELECT_ELEVE_PAR_NOM = "SELECT * FROM eleves WHERE nom= ?";
 
 	public Eleve addeleve(final String prenom, final String nom,
-			final Classe classe) throws SQLException {
+			final String classe) throws SQLException {
 		final PreparedStatement preparedStatement = ServicesDAO
 				.preparedStatement(INSERT_ELEVES);
 		preparedStatement.setString(1, prenom);
 		preparedStatement.setString(2, nom);
-		preparedStatement.setObject(3, classe);
+		preparedStatement.setString(3, classe);
 		preparedStatement.executeUpdate();
 
 		return new Eleve(prenom, nom, classe);
 	}
 
-	public List<Eleve> listEleves() throws SQLException {
+	public ArrayList<Eleve> listEleves() throws SQLException {
+		ArrayList<Eleve> liste = new ArrayList<Eleve>();
 		final PreparedStatement preparedStatement = ServicesDAO
 				.preparedStatement(SELECT_ELEVES);
 		final ResultSet rst = preparedStatement.executeQuery();
-		return null;
+		while (rst.next()) {
+			String prenom = rst.getString("prenom");
+			String nom = rst.getString("nom");
+			String classe = rst.getString("classe");
+			liste.add(new Eleve(prenom, nom, classe));
+
+		}
+		return liste;
 	}
+
+	public Eleve modifeleve(final String prenomorigin, final String prenomodif,
+			final String nomorigin, final String nommodif,
+			final String classeorigin, final String classemodif)
+					throws SQLException {
+		{
+			final PreparedStatement prepareStatement = ServicesDAO
+					.preparedStatement(UPDATE_ELEVES);
+			prepareStatement.setString(1, prenomodif);
+			prepareStatement.setString(2, nommodif);
+			prepareStatement.setString(3, classemodif);
+			prepareStatement.executeUpdate();
+			return new Eleve(prenomodif, nommodif, classemodif);
+		}
+
+	}
+
 }
