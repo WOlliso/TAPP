1de12d0f234ecb5713635069ece3c0ecf14de5aa
William Olliso <will.olliso2@gmail.com>
2015-05-22 12:00:25 +0200

    Removed double implementation of interface

--- tappweb/src/main/java/org/tapp/dal/JournalDAO.java
+++ tappweb/src/main/java/org/tapp/dal/JournalDAO.java
@@ -1,7 +1,11 @@
 package org.tapp.dal;
 
+import java.util.ArrayList;
 
-public class JournalDAO extends ServicesDAO implements InjectionDAOInter {
+import org.tapp.bll.Document;
+
+
+public class JournalDAO extends ServicesDAO  {
 
 	public static final String INSERT_CAHIER_JOURNALS = "INSERT INTO cahier_journal (nom_memo,date_memo) VALUES(?,?)";
 	public static final String SELECT_CAHIER_JOURNALS = "SELECT * FROM cahier_journal";
@@ -9,4 +13,22 @@ public class JournalDAO extends ServicesDAO implements InjectionDAOInter {
 	public static final String UPDATE_CAHIER_JOURNALS = "UPDATE cahier_journal SET nom_memo = ? WHERE nom_memo = ?";
 	public static final String DELETE_CAHIER_JOURNALS = "DELETE FROM cahier_journal WHERE nom_memo = ?";
 	public static final String DELETE_CAHIER_JOURNALS_DATE = "DELETE FROM cahier_journal WHERE date_memo = ?";
+	
+	
+	
+//	@Override
+//	public ArrayList<Document> Listedocs() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//	@Override
+//	public void ajoutdoc(String nom) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//	@Override
+//	public Document selectdoc(String nom) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
 }
